718477d3819b1618dd70d5ac196416537e5f83d9
Akshith Martis <martisakshith@gmail.com>
2024-08-19 22:15:44 +0530

    Akshara-Resort

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+<div>
+         
+            <div class="footer">
+                    <div class="clearfix"> </div>
+                        <p style="font-family: cursive;">Akshara Resort© @ 2022 </p>
+            </div>
+        </div>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div class="footer-section">
+						<div class="container">
+							<div class="footer-top">
+								<p>Akshara Resort @ 2022 </p>
+							</div>
+						
+				</div>
+			</div>
